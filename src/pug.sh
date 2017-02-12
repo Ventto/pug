a2ab3f889e845bc1297d58fd5ab0f2fe61c83ade
Thomas Venries <thomas.venries@gmail.com>
2017-02-12 22:46:00 +0100

    src: Removed space.

--- src/pug.sh
+++ src/pug.sh
@@ -41,7 +41,7 @@ pug_install() {
 }
 
 pug_update() {
-    echo "${bold}${cyan}:: ${white} Processing gists update...${normal}"
+    echo "${bold}${cyan}::${white} Processing gists update...${normal}"
 
     if ! pacman -Qqen | gist -u "${GIST_NAT}" -f "${PACMANFILE}"; then exit 1; fi
     if ! pacman -Qqem | gist -u "${GIST_AUR}" -f "${AURFILE}";    then exit 1; fi

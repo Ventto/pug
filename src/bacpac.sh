caac234856c4b157f4a11f64f7f29ffe5308618a
Thomas Venries <thomas.venries@gmail.com>
2017-02-12 18:25:28 +0100

    src: Added makepkg's theme to output.

--- src/bacpac.sh
+++ src/bacpac.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if test -t 1; then
+    ncolors=$(tput colors)
+    if test -n "$ncolors"; then
+        normal="$(tput sgr0)"
+        bold="$(tput bold)"
+        green="$(tput setaf 2)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
+fi
+
 bacpac_install() {
     [ -n "${1}" ] && [ ! -d "${1}" ] && exit 1
 
@@ -8,35 +19,46 @@ bacpac_install() {
     if ! gist --login ; then exit 1; fi
 
     mkdir -p "${pkgdir}/root";
-    cp ~/.gist "${pkgdir}/root/.gist";
 
-    echo "1: Creating installed packages lists.";
+    if ! cmp --silent ~/.gist "${pkgdir}/root/.gist"; then
+        cp ~/.gist "${pkgdir}/root/.gist";
+    fi
+
+    echo
+    echo "${bold}${green}==>${white} Saving installed package lists to gists..."
+    echo "${bold}${cyan}  ->${white} Creating packages lists..."
+    echo "${bold}${cyan}  ->${white} Generating gist links..."
 
     GIST_NAT=$(pacman -Qqen | gist -p -f native-list.pkg)
     GIST_AUR=$(pacman -Qqem | gist -p -f aur-list.pkg)
 
-    echo "2: Gist links:"
-    echo -e "GIST_NAT=${GIST_NAT}\nGIST_AUR=${GIST_AUR}" | \
+    echo "GIST_NAT=${GIST_NAT}" | \
         sed 's/https:\/\/gist.github.com\///g' >> "${pkgdir}/etc/bacpac";
-    echo -e "${GIST_NAT}\n${GIST_AUR}"
+    echo "GIST_AUR=${GIST_AUR}" | \
+        sed 's/https:\/\/gist.github.com\///g' >> "${pkgdir}/etc/bacpac";
+    echo "    ${GIST_NAT}"
+    echo "    ${GIST_AUR}"
+    echo
 }
 
 bacpac_update() {
-    echo 'bacpac: updating package list...';
+    echo "${bold}[bacpac]"
+    echo "${bold}${cyan}:: ${white} Updating package list gists...${normal}"
+
     if ! pacman -Qqen | gist -u "${GIST_NAT}" -f native-list.pkg ; then exit 1; fi
     if ! pacman -Qqem | gist -u "${GIST_AUR}" -f aur-list.pkg;     then exit 1; fi
 }
 
 bacpac() {
-    # Add Ruby to PATH
     PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 
-    # Load config
-    [[ -r '/etc/bacpac' ]] && source "${pkgdir}/etc/bacpac"
+    test -r '/etc/bacpac' && source "${pkgdir}/etc/bacpac"
 
     # Determine if fresh install is needed
-    if [ -z "${GIST_NAT}" ] || [ -z "${GIST_AUR}" ]; then
-        echo "bacpac: fresh install detected."
+    if test -z "${GIST_NAT}" || test -z "${GIST_AUR}"; then
+        echo "${bold}[bacpac]"
+        echo "${cyan}:: ${white}Fresh install is needed.${normal}"
+        echo
     else
         bacpac_update;
     fi

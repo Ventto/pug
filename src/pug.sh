daeca14953b42f1da07c5604e4404a996293bdbf
Thomas Venries <thomas.venries@gmail.com>
2017-05-23 00:22:02 +0200

    misc: Fix write-redirection if manual reinstallation.

--- src/pug.sh
+++ src/pug.sh
@@ -35,7 +35,7 @@ pug_install() {
     GIST_AUR=$(pacman -Qqem | gist -p -f "${AURFILE}" -d 'AUR package list.')
 
     echo "GIST_NAT=${GIST_NAT}" | \
-        sed 's/https:\/\/gist.github.com\///g' >> "${pkgdir}/etc/pug";
+        sed 's/https:\/\/gist.github.com\///g' > "${pkgdir}/etc/pug";
     echo "GIST_AUR=${GIST_AUR}" | \
         sed 's/https:\/\/gist.github.com\///g' >> "${pkgdir}/etc/pug";
 
